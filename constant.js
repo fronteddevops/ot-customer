98ea541a10cb3ac70e4376ea92cf7e68dd6bb187
fronteddevops <114234857+fronteddevops@users.noreply.github.com>
2025-02-19 12:23:49 +0530

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,13 +1,5 @@
 export default {
-  // BASE_URL: 'http://localhost:3003/v1',
-  BASE_URL: `http://192.168.29.238:3003/v1`,
-  
-  //BASE_URL: "https://api.obeytina.com/v1",
-  //APP_URL: "https://www.obeytina.com",
-  // S3_BUCKET_IMAGE: "ot-dev-media-convert-destination920a3c57-aljc201n45zh",
-  //S3_BUCKET_VIDEO: "ot-dev-media-convert-source71e471f1-q124x5s81xth",
-  //REGION: "us-east-1",
-  // CLOUDFRONT_DESTINATION: "https://d39rqdu9os3h2h.cloudfront.net/",
+  BASE_URL: `https://app.applatus.com:9003/v1`,
   CLOUDFRONT_DESTINATION: "http://192.168.29.238:3003/uploads/",
   STRIPE_PK:"pk_live_51Jr[p,rrrrrrkpojkojopjojoojmZ1HJRTPwlxRSZCGDUgftufgytufgvhG5MVneD4BlEuuiihplg1K32y1Dt9gp93FilZsrhZcV8ZjORfqRgPmPwkSKnNL7sPwiL900ESVImMN6"
 };
