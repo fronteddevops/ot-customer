ffd9dd5a4f4a0e7771042dd94c5718f9bfe7549c
unknown <applatus.sandeep@gmail.com>
2025-02-24 13:22:56 -0100

    img base url

--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
 export default {
   BASE_URL: `https://app.applatus.com:9003/v1`,
-  CLOUDFRONT_DESTINATION: "http://192.168.29.238:3003/uploads/",
+  CLOUDFRONT_DESTINATION: "https://app.applatus.com:9003/uploads/",
   STRIPE_PK:"pk_live_51Jr[p,rrrrrrkpojkojopjojoojmZ1HJRTPwlxRSZCGDUgftufgytufgvhG5MVneD4BlEuuiihplg1K32y1Dt9gp93FilZsrhZcV8ZjORfqRgPmPwkSKnNL7sPwiL900ESVImMN6"
 };
